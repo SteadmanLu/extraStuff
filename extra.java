dcc35067eb546d0da008515f19d4ae382a0d18f2
SteadmanLu <steadl2@rpi.edu>
2026-01-28 20:47:55 -0500

    Update extra.java

--- extra.java
+++ extra.java
@@ -1,3 +1,6 @@
 public class extra {
-
+    public static void main(String[] args) {
+        System.out.println("Hello from Person A");
+        System.out.println("Hello from Person B");
+    }
 }
